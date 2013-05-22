c13c0df4fb3b76978b1e83e5197e298e0c08df37
kangax <kangax@gmail.com>
2013-05-22 15:08:58 +0200

    Update fabric, docs, functional tests

--- test/functional/functional_tests.js
+++ test/functional/functional_tests.js
@@ -1,6 +1,6 @@
 (function(){
   this.canvas = new fabric.Canvas('testee0');
-  
+
   canvas.add(new fabric.Line([10,10,40,50], { stroke: 'red' }));
   canvas.add(new fabric.Line([70,80,110,110], { stroke: 'green', strokeWidth: 7 }));
   canvas.add(new fabric.Line([120,40,50,170], { stroke: 'blue', strokeWidth: 3 }));
@@ -9,9 +9,9 @@
 
 (function(){
   this.canvas = new fabric.Canvas('testee1');
-  
+
   var width = height = 40;
-  
+
   canvas.add(new fabric.Rect({
     fill: 'red', left: 50, top: 50, width: width, height: height
   }));
@@ -26,9 +26,9 @@
 
 (function(){
   this.canvas2 = new fabric.Canvas('testee2');
-  
+
   var radius = 25;
-  
+
   canvas2.add(new fabric.Circle({
     fill: 'red', left: 100, top: 100, radius: radius
   }));
@@ -43,10 +43,10 @@
 
 (function(){
   this.canvas3 = new fabric.Canvas('testee3');
-  
+
   var rx = 20,
       ry = 40;
-  
+
   canvas3.add(new fabric.Ellipse({
     fill: 'red', left: 100, top: 100, rx: rx, ry: ry
   }));
@@ -61,7 +61,7 @@
 
 (function(){
   this.canvas4 = new fabric.Canvas('testee4');
-  
+
   var points = [
     {x:10,y:10},
     {x:15,y:15},
@@ -70,7 +70,7 @@
     {x:30,y:0},
     {x:-10,y:-10}
   ];
-  
+
   canvas4.add(new fabric.Polygon(points, {
     fill: 'red', left: 100, top: 100
   }));
@@ -85,7 +85,7 @@
 
 (function(){
   this.canvas5 = new fabric.Canvas('testee5');
-  
+
   var points = [
     {x:10,y:10},
     {x:15,y:15},
@@ -94,7 +94,7 @@
     {x:30,y:0},
     {x:-10,y:-10}
   ];
-  
+
   canvas5.add(new fabric.Polyline(points, {
     stroke: 'red', left: 100, top: 100
   }));
@@ -109,11 +109,14 @@
 
 // using onload to ensure all images are loaded
 window.onload = function() {
-  
+
   var benchmarks = window.__benchmarks = [];
   var imgEls = document.getElementsByTagName('img');
-  
+
   fabric.util.toArray(imgEls).forEach(function(imgElement, i) {
+
+    if (!imgElement.alt) return;
+
     fabric.util.request('../W3C_SVG_12_TinyTestSuite_beta/svg/' + imgElement.alt + '.svg', {
       method: 'GET',
       onComplete: function(r) {
@@ -121,10 +124,10 @@ window.onload = function() {
         if (!r.responseXML) return;
         var doc = r.responseXML.documentElement;
         if (!doc) return;
-        
+
         var startTime = new Date();
-        
-        fabric.parseSVGDocument(doc, function(elements) {
+
+        fabric.parseSVGDocument(doc, function(objects) {
           var dimensions = {
             width: imgElement.width,
             height: imgElement.height
@@ -135,13 +138,11 @@ window.onload = function() {
             height: dimensions.height + 'px'
           });
           imgElement.parentNode.insertBefore(canvasElement, imgElement.nextSibling);
-          
+
           var oCanvas = window.__canvas = new fabric.Canvas(canvasElement);
-          var group = new fabric.PathGroup(elements);
-          
-          oCanvas.add(group);
+          oCanvas.add.apply(oCanvas, objects)
         });
-        
+
         benchmarks.push(new Date() - startTime);
       }
     });

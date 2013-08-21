49bdb5c1b1f534e1238ad4d3ec02aad3cb07a6f8
kangax <kangax@gmail.com>
2013-08-21 13:14:18 +0200

    Some header tweaks

--- js/master.js
+++ js/master.js
@@ -3,6 +3,8 @@ _gaq.push(['_setAccount', 'UA-1128111-28']);
 _gaq.push(['_trackPageview']);
 
 (function() {
+  if (document.location.hostname === 'localhost') return;
+
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);

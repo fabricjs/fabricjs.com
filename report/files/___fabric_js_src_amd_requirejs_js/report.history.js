61b1b13e8e88eeb02fd33f04d67dd4fa7791c788
kangax <kangax@gmail.com>
2013-09-04 15:54:04 +0200

    Update report

---
+++ report/files/___fabric_js_src_amd_requirejs_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Tue, 03 Sep 2013 19:31:06 GMT","sloc":5,"lloc":4,"functions":1,"deliveredBugs":0.031337215116252606,"maintainability":100,"lintErrors":1,"difficulty":8.571428571428571}]

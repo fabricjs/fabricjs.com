40fb5546c6604d0e087b4f2851366b097977c3dc
kangax <kangax@gmail.com>
2013-11-16 17:53:39 +0100

    Update kitchensink

--- js/kitchensink.js
+++ js/kitchensink.js
@@ -1007,13 +1007,50 @@
     Cufon.fonts.delicious.offsetTop = 25;
   }
 
-  var iText2 = new fabric.IText('lorem ipsum\ndolor\nsit amet', {
+  var iText2 = new fabric.IText('lorem ipsum\ndolor\nsit amet\nconsectetur', {
     left: 100,
     top: 150,
     fontFamily: 'Helvetica',
     fill: '#333',
-    lineHeight: 1,
-    angle: -20
+    angle: -10,
+    styles: {
+      0: {
+        0: { fill: 'red', fontSize: 20 },
+        1: { fill: 'red', fontSize: 30 },
+        2: { fill: 'red', fontSize: 40 },
+        3: { fill: 'red', fontSize: 50 },
+        4: { fill: 'red', fontSize: 60 },
+
+        6: { textBackgroundColor: 'yellow' },
+        7: { textBackgroundColor: 'yellow' },
+        8: { textBackgroundColor: 'yellow' },
+        9: { textBackgroundColor: 'yellow' }
+      },
+      1: {
+        0: { textDecoration: 'underline' },
+        1: { textDecoration: 'underline' },
+        2: { fill: 'green', fontStyle: 'italic', textDecoration: 'underline' },
+        3: { fill: 'green', fontStyle: 'italic', textDecoration: 'underline' },
+        4: { fill: 'green', fontStyle: 'italic', textDecoration: 'underline' }
+      },
+      2: {
+        0: { fill: 'blue', fontWeight: 'bold' },
+        1: { fill: 'blue', fontWeight: 'bold' },
+        2: { fill: 'blue', fontWeight: 'bold' },
+
+        4: { fontFamily: 'Courier', textDecoration: 'line-through' },
+        5: { fontFamily: 'Courier', textDecoration: 'line-through' },
+        6: { fontFamily: 'Courier', textDecoration: 'line-through' },
+        7: { fontFamily: 'Courier', textDecoration: 'line-through' }
+      },
+      3: {
+        0: { fontFamily: 'Impact', fill: '#666' },
+        1: { fontFamily: 'Impact', fill: '#666' },
+        2: { fontFamily: 'Impact', fill: '#666' },
+        3: { fontFamily: 'Impact', fill: '#666' },
+        4: { fontFamily: 'Impact', fill: '#666' }
+      }
+    }
   });
   canvas.add(iText2);
 

38f07b56a554288a6445115d1e03e09170db03ad
Andrea Bogazzi <andreabogazzi79@gmail.com>
2017-11-25 12:55:01 +0100

    small improvements to dos

--- build_docs.js
+++ build_docs.js
@@ -1,5 +1,5 @@
 var exec = require('child_process').exec;
-var command = 'jsdoc lib/fabric.js README.md -c jsdoc.json';
+var command = 'jsdoc lib/fabric.js lib/package.json README.md  -c jsdoc.json';
 
 console.log('Running:', command);
 

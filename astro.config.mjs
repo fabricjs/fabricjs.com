ea62e542072ee817b325f2a3c1094d1504201ba5
Andrea Bogazzi <abogazzi@shutterstock.com>
2024-03-22 14:47:07 +0100

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import starlight from "@astrojs/starlight";
+import starlightTypeDoc, { typeDocSidebarGroup } from 'starlight-typedoc';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
     title: 'FabricJS Docs and Guides',
+    plugins: [
+      // Generate the documentation.
+      starlightTypeDoc({
+        entryPoints: ['../node_modules/fabric/fabric.ts'],
+        tsconfig: '../node_modules/fabric/typedoc.config.json',
+      }),
+    ],
   }), react()]
 });

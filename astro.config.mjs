84a59c1aceb8116899e1c546df42690e20e51e23
Andrea Bogazzi <abogazzi@shutterstock.com>
2024-03-22 13:48:03 +0100

    starlight compiles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  integrations: [starlight({
+    title: 'FabricJS Docs and Guides',
+  }), react()]
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,7 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content';
+import { docsSchema } from '@astrojs/starlight/schema';
+
 // 2. Define your collection(s)
 const demosCollection = defineCollection({
   type: 'content',
@@ -11,19 +13,14 @@ const demosCollection = defineCollection({
   }),
 });
 
-const guidesCollection = defineCollection({
+const docsCollection = defineCollection({
     type: 'content',
-    schema: z.object({
-      date: z.string(),
-      title: z.string(),
-      tags: z.array(z.string()).optional(),
-      description: z.string(),
-    }),
+    schema: docsSchema(),
   });
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   demo: demosCollection,
-  guides: guidesCollection,
+  docs: docsCollection,
 };
